55e4ccce4f97b3770a8ea729a8b2ab5f1b17b482
narcieliton <narcieliton87@gmail.com>
2021-08-08 21:10:11 -0300

    add apipropertie in field amountOfMovie

--- src/main/java/com/narcielitonlopes/starwars/domain/dto/PlanetDto.java
+++ src/main/java/com/narcielitonlopes/starwars/domain/dto/PlanetDto.java
@@ -1,5 +1,6 @@
 package com.narcielitonlopes.starwars.domain.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,5 +16,6 @@ public class PlanetDto {
     private String name;
     private String climate;
     private String terrain;
+    @ApiModelProperty(value = "Este campo será preenchido atraves do consumo da API publica https://swapi.dev/ ao salvar um planeta")
     private int amountOfMovie;
 }
